953f008d0175dce39017049fa2fc504fdeb0b523
jjorissen52 <jjorissen52@gmail.com>
2019-04-29 18:28:57 -0500

    remove unused kwargs from test, added gitignore and requirements files

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,15 +13,15 @@ class TestBaseModel(TestCase):
             test_field = models.Field(required=True)
 
         # should be no issues here
-        test = MyModel(test_field=7, _connect_client=False)
+        test = MyModel(test_field=7)
 
         # by default we do not validate until Field.validate is called, so this does not complain about the
         # required field
-        test = MyModel(test_field=None, _connect_client=False)
+        test = MyModel(test_field=None)
 
         # however, if we choose to validate on init, an error will be raised for None when required=True
         with self.assertRaises((models.ValidationError, )):
-            test = MyModel(test_field=None, _validate_on_init=True, _connect_client=False)
+            test = MyModel(test_field=None, _validate_on_init=True)
 
     def test__get_fields(self):
 
@@ -30,7 +30,7 @@ class TestBaseModel(TestCase):
             test_field2 = models.Field()
             test_field3 = models.Field()
 
-        test = MyModel(_connect_client=False)
+        test = MyModel()
         # fields defined in the class scope are stored privately and can be listed by calling _get_fields
         self.assertTrue(all([field in test._get_fields() for field in ['_test_field1', '_test_field2', '_test_field3']]))
 
@@ -43,18 +43,18 @@ class TestBaseModel(TestCase):
             test_field4 = models.Field(required=True, validation=lambda x: isinstance(x, int))
 
         # required fields are... required
-        MyModel(test_field1=1, test_field4=7, _validate_on_init=True, _connect_client=False)  # this is okay
+        MyModel(test_field1=1, test_field4=7, _validate_on_init=True)  # this is okay
         with self.assertRaises((models.ValidationError, )):
-            MyModel(_validate_on_init=True, _connect_client=False)  # this is not.
+            MyModel(_validate_on_init=True)  # this is not.
 
         # defaults should work
-        defaults_work = MyModel(_connect_client=False)
+        defaults_work = MyModel()
         self.assertAlmostEqual(defaults_work.test_field2, time.time(), 2, 'defaults do not work.')
 
         # validation can pass and fail
-        MyModel(test_field1=1, test_field4=7, _validate_on_init=True, _connect_client=False)
+        MyModel(test_field1=1, test_field4=7, _validate_on_init=True)
         with self.assertRaises((models.ValidationError, )):
-            MyModel(test_field1=1, test_field4=7.0, _validate_on_init=True, _connect_client=False)
+            MyModel(test_field1=1, test_field4=7.0, _validate_on_init=True)
 
     def test_to_dict(self):
 
@@ -64,7 +64,7 @@ class TestBaseModel(TestCase):
             three = models.Field(default=3)
             four = models.Field(default=4)
 
-        my_instance = MyModel(_connect_client=False)
+        my_instance = MyModel()
         my_instance_dict = my_instance.to_dict()
 
         for i, key in enumerate(['one', 'two', 'three', 'four']):
